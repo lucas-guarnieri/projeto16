26e79e6963c76aeaf653a5a8931cbc66f15e9241
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-06-10 13:22:36 -0300

    prepare for deploy

--- db.js
+++ db.js
@@ -1,22 +1,39 @@
+// import pg from 'pg';
+// import dotenv from "dotenv";
+
+// const { Pool } = pg;
+// dotenv.config();
+
+// const user = 'postgres';
+// const password = process.env.DB_PASSWORD;
+// const host = process.env.HOST;
+// const port = 5432;
+// const database = 'projeto16';
+
+// const connection = new Pool({
+//   user,
+//   password,
+//   host,
+//   port,
+//   database
+// });
+
+
+// export default connection;
+
 import pg from 'pg';
 import dotenv from "dotenv";
 
 const { Pool } = pg;
 dotenv.config();
 
-const user = 'postgres';
-const password = process.env.DB_PASSWORD;
-const host = process.env.HOST;
-const port = 5432;
-const database = 'projeto16';
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
 
-const connection = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
-});
-
+const connection = new Pool(databaseConfig);
 
 export default connection;
